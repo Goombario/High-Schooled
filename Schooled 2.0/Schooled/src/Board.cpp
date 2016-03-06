a1a129c8d0ac856a249b63dbb360085e5b0067ae
Graham Watson <12705535+Goombario@users.noreply.github.com>
2016-03-06 15:30:48 -0500

    [NEW] Calculates closest path [TODO] Put pathing in different class

--- Schooled 2.0/Schooled/src/Board.cpp
+++ Schooled 2.0/Schooled/src/Board.cpp
@@ -19,12 +19,14 @@ namespace Board
 				boardTiles[w][h].isPath = false;
 			}
 		}
+
+		clearLeePath();
 	}
 
 	void Board::placeToken(int location)
 	{
-		boardTiles[location / Stage::BOARD_WIDTH]
-		[location % Stage::BOARD_HEIGHT].hasToken = true;
+		boardTiles[location % Stage::BOARD_WIDTH]
+		[location / Stage::BOARD_HEIGHT].hasToken = true;
 	}
 
 	void Board::placeToken(int w, int h)
@@ -34,8 +36,8 @@ namespace Board
 
 	void Board::removeToken(int location)
 	{
-		boardTiles[location / Stage::BOARD_WIDTH]
-			[location % Stage::BOARD_HEIGHT].hasToken = false;
+		boardTiles[location % Stage::BOARD_WIDTH]
+			[location / Stage::BOARD_HEIGHT].hasToken = false;
 	}
 
 	void Board::removeToken(int w, int h)
@@ -134,22 +136,120 @@ namespace Board
 	int Board::updatePath()
 	{
 		clearPath();
-		getTile(firstPos).isPath = true;
+		
+		clearLeePath();
+		waveMap.wMap[firstPos % Stage::BOARD_WIDTH][firstPos / Stage::BOARD_HEIGHT] = 0;
 
-		// Create the path to the current pos from the first pos
+		// Set up the waveMap
+		int distance = getLeePath(0);
+		
+		tracePath(getPlayerlocation() % Stage::BOARD_WIDTH, getPlayerlocation() / Stage::BOARD_HEIGHT);
+
+		std::cout << "Distance :" << distance << std::endl;
 		for (int w = 0; w < Stage::BOARD_WIDTH; w++)
 		{
-			if (getPlayerlocation() / Stage::BOARD_WIDTH < firstPos / Stage::BOARD_WIDTH ||
-				getPlayerlocation() / Stage::BOARD_WIDTH)
-			{
-
-			}
 			for (int h = 0; h < Stage::BOARD_HEIGHT; h++)
 			{
-				boardTiles[w][h].isPath = false;
+				std::cout << waveMap.wMap[w][h];
+			}
+			std::cout << std::endl;
+		}
+
+		for (int w = 0; w < Stage::BOARD_WIDTH; w++)
+		{
+			for (int h = 0; h < Stage::BOARD_HEIGHT; h++)
+			{
+				std::cout << boardTiles[w][h].isPath;
+			}
+			std::cout << std::endl;
+		}
+		return distance;
+	}
+
+	void Board::tracePath(int w, int h)
+	{
+		boardTiles[w][h].isPath = true;
+
+		// IF in bounds AND set value AND less than current node
+		if (inBounds(w + 1, h) && waveMap.wMap[w + 1][h] >= 0 && waveMap.wMap[w + 1][h] < waveMap.wMap[w][h])
+		{
+			tracePath(w + 1, h);
+		}
+		else if (inBounds(w - 1, h) && waveMap.wMap[w - 1][h] >= 0 && waveMap.wMap[w - 1][h] < waveMap.wMap[w][h])
+		{
+			tracePath(w - 1, h);
+		}
+		else if (inBounds(w, h + 1) && waveMap.wMap[w][h + 1] >= 0 && waveMap.wMap[w][h + 1] < waveMap.wMap[w][h])
+		{
+			tracePath(w, h + 1);
+		}
+		else if (inBounds(w, h - 1) && waveMap.wMap[w][h - 1] >= 0 && waveMap.wMap[w][h - 1] < waveMap.wMap[w][h])
+		{
+			tracePath(w, h - 1);
+		}
+	}
+
+	void Board::clearLeePath()
+	{
+		for (int w = 0; w < Stage::BOARD_WIDTH; w++)
+		{
+			for (int h = 0; h < Stage::BOARD_HEIGHT; h++)
+			{
+				waveMap.wMap[w][h] = -1;
+			}
+		}
+		waveMap.foundTarget = false;
+	}
+
+	bool Board::inBounds(int w, int h)
+	{
+		if (w < Stage::BOARD_WIDTH && w >= 0 &&
+			h < Stage::BOARD_HEIGHT && h >= 0)
+			return true;
+		return false;
+	}
+
+	void Board::setNeighbours(int w, int h, int i)
+	{
+		// Check neighbours and update their values
+		// IF within bounds AND not labeled AND passable
+		if (inBounds(w + 1, h) && waveMap.wMap[w + 1][h] < 0 && boardTiles[w + 1][h].isPassable)
+		{
+			waveMap.wMap[w + 1][h] = i + 1;
+		}
+		if (inBounds(w - 1, h) && waveMap.wMap[w - 1][h] < 0 && boardTiles[w - 1][h].isPassable)
+		{
+			waveMap.wMap[w - 1][h] = i + 1;
+		}
+		if (inBounds(w, h + 1) && waveMap.wMap[w][h + 1] < 0 && boardTiles[w][h + 1].isPassable)
+		{
+			waveMap.wMap[w][h + 1] = i + 1;
+		}
+		if (inBounds(w, h - 1) && waveMap.wMap[w][h - 1] < 0 && boardTiles[w][h - 1].isPassable)
+		{
+			waveMap.wMap[w][h - 1] = i + 1;
+		}
+	}
+
+	int Board::getLeePath(int i)
+	{
+		for (int w = 0; w < Stage::BOARD_WIDTH; w++)
+		{
+			for (int h = 0; h < Stage::BOARD_HEIGHT; h++)
+			{
+				// If at the target, set foundTarget to true
+				if (w + h*Stage::BOARD_HEIGHT == getPlayerlocation() && waveMap.wMap[w][h] >= 0)
+				{
+					waveMap.foundTarget = true;
+					return waveMap.wMap[w][h];
+				}
+
+				// Sets all neighbors of the i value to be i+1
+				if (waveMap.wMap[w][h] == i)
+					setNeighbours(w, h, i);
 			}
 		}
 
-		return 0;
+		return getLeePath(i + 1);
 	}
 }
