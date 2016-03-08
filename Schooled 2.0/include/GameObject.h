d6e7c9828475caabb36505cd3d6c6cbddf0d64a4
Graham <Graham@localhost>
2016-03-08 16:33:12 -0500

    [ADD] Game Object

--- Schooled 2.0/include/GameObject.h
+++ Schooled 2.0/include/GameObject.h
@@ -10,16 +10,34 @@ namespace GameObject
 	{
 	public:
 		GameObject();
+
 		void firstOrder();
 		void secondOrder();
+		void euler();
+		void adamsBashford();
 
 		void impulse(Vector2 const& force);
 
+		Vector2 getPos() const { return position; }
+		Vector2 getVelocity() const { return velocity; }
+		Vector2 getAcceleration() const { return acceleration; }
+
+		void setPos(Vector2 const& newPos) { position = newPos; }
+		void setVelocity(Vector2 const& newVel) { velocity = newVel; }
+		void setAcceleration(Vector2 const& newAcc) { acceleration = newAcc; }
+	
 	private:
+		Vector2 getPrevVelocity() const { return prevVelocity; }
+		Vector2 getPrevAcceleration() const { return prevAcceleration; }
+		void setPrevVelocity(Vector2 const& newPrevVelocity) { prevVelocity = newPrevVelocity; }
+		void setPrevAcceleration(Vector2 const& newPrevAcceleration) { prevAcceleration = newPrevAcceleration; }
+
 		double mass;
 		Vector2 position;
 		Vector2 velocity;
+		Vector2 prevVelocity;
 		Vector2 acceleration;
+		Vector2 prevAcceleration;
 	};
 }
 
